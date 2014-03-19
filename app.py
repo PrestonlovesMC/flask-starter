1dbd9915121a397d28fbc503e561362da51fb484
Preston <prestonvanasdlen@gmail.com>
2014-03-18 19:38:19 -0500

    tip calc working

--- app.py
+++ app.py
@@ -18,7 +18,15 @@ def tip_calc():
     if request.method == 'GET':
         return render_template('tip-calc.html')
     elif request.method == 'POST':
-        return render_template('tip-calc.html')
+		tip_perc = float(request.form['tip-perc'])
+		meal_price = float(request.form['meal-price'])
+		tax_rate = float(request.form['tax-rate'])
+		meal_plus_tax = meal_price + meal_price * tax_rate
+		return render_template('tip-calc.html',
+			tip_perc=tip_perc,
+			meal_price=meal_price,
+			tax_rate=tax_rate,
+			meal_plus_tax=meal_plus_tax)
 							   
 @app.route('/my-new-page')
 def my_new_function():
